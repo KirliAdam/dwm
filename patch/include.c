5b4177c9059b365c0dfb1324affcc7eea9635443
Yasin Şen <yasin@tutamail.com>
2020-10-25 21:09:33 +0300

    My build of dwm

---
+++ patch/include.c
@@ -0,0 +1,24 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+
+#include "bar_dwmblocks.c"
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_statuscmd.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+#include "bar_flexwintitle.c"
+#include "bar_systray.c"
+#include "bar_wintitleactions.c"
+
+/* Other patches */
+#include "cyclelayouts.c"
+#include "killunsel.c"
+#include "pertag.c"
+#include "rotatestack.c"
+#include "scratchpad.c"
+#include "sticky.c"
+#include "vanitygaps.c"
+/* Layouts */
+#include "layout_flextile-deluxe.c"
---
+++ patch/include.h
@@ -0,0 +1,24 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+
+#include "bar_dwmblocks.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_statuscmd.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_flexwintitle.h"
+#include "bar_systray.h"
+#include "bar_wintitleactions.h"
+
+/* Other patches */
+#include "cyclelayouts.h"
+#include "killunsel.h"
+#include "pertag.h"
+#include "rotatestack.h"
+#include "scratchpad.h"
+#include "sticky.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_flextile-deluxe.h"
